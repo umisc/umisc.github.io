31395dfa9215bc608bc4ab89320ecf76b108322c
chuahx <xinyu.chuah@student.unimelb.edu.au>
2022-05-23 13:36:31 +1000

    Updated Sponsors 2022

--- src/pages/sponsers.js
+++ src/pages/sponsers.js
@@ -1,86 +1,136 @@
-import React from "react";
+/* eslint-disable linebreak-style */
+import React from 'react'
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import atlassian_logo from "../images/atlassian-logo.png";
-import cybersecpeople_logo from "../images/cybersecpeople-logo.png";
-import dronesec_logo from "../images/dronesec-logo-transparent.png";
-import kaspersky_logo from "../images/Kaspersky logo white (no background).png";
-import commbank_logo from "../images/CommBank-Logo.png";
-import optiver_logo from "../images/optiver-logo.png";
-import accenture_logo from "../images/accenture-logo.png";
-import puresecurity_logo from "../images/Pure Security Transparent.png";
-import pentesterlab_logo from "../images/pentesterlab-logo-transparent.png";
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
-import "../styles/sponsers.sass";
+// Diamond
+import atlassian_logo from '../images/atlassian-logo.png'
+import afterpay_logo from '../images/afterpay-logo.png'
+import commbank_logo from '../images/CommBank-Logo.png'
+import tesserent_logo from '../images/Tesserent-logo.png'
+import fortian_logo from '../images/fortian-logo.png'
+
+// Silver
+import cybersecpeople_logo from '../images/cybersecpeople-logo.png'
+import promithicio_logo from '../images/promithicio-logo.png'
+
+// Gold
+import optiver_logo from '../images/optiver-logo.jpg'
+import canva_logo from '../images/canva-logo.png'
+import sekuro_logo from '../images/sekuro-logo.jpg'
+
+// Old Sponsors
+// import dronesec_logo from '../images/dronesec-logo-transparent.png'
+// import kaspersky_logo from '../images/Kaspersky logo white (no background).png'
+// import accenture_logo from '../images/accenture-logo.png'
+// import puresecurity_logo from '../images/Pure Security Transparent.png'
+// import pentesterlab_logo from '../images/pentesterlab-logo-transparent.png'
+
+import '../styles/sponsers.sass'
 
 const SponsersPage = () => {
-  return (
-    <Layout>
-      <SEO title="Sponsors" />
-      <div className="gallery-container">
-        <h1 className="gold-heading">Gold Sponsors</h1>
-        <div>
-          <div className="sponsors-container">
-            <div className="sponsors-logos">
-              <a href="https://www.atlassian.com/">
-                <img src={atlassian_logo} className="atlassian-logo" />
-              </a>
+    return (
+        <Layout>
+            <SEO title="Sponsors" />
+            <div className="gallery-container">
+                <h1 className="diamond-heading">Diamond Sponsors</h1>
+                <div>
+                    <div className="sponsors-container">
+                        <div className="sponsors-logos">
+                            <a href="https://www.atlassian.com/">
+                                <img
+                                    src={atlassian_logo}
+                                    className="atlassian-logo"
+                                />
+                            </a>
+                        </div>
+                        <div className="sponsors-logos">
+                            <a href="https://www.afterpay.com/">
+                                <img
+                                    src={afterpay_logo}
+                                    className="afterpay-logo"
+                                />
+                            </a>
+                        </div>
+                        <div className="sponsors-logos">
+                            <a href="https://www.commbank.com.au/">
+                                <img
+                                    src={commbank_logo}
+                                    className="commbank-logo"
+                                />
+                            </a>
+                        </div>
+                        <div className="sponsors-logos">
+                            <a href="https://tesserent.com/">
+                                <img
+                                    src={tesserent_logo}
+                                    className="tesserent-logo"
+                                />
+                            </a>
+                        </div>
+                        <div className="sponsors-logos">
+                            <a href="https://www.fortian.com.au">
+                                <img
+                                    src={fortian_logo}
+                                    className="fortian-logo"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <h1 className="gold-heading">Gold Sponsors</h1>
+                <div>
+                    <div className="sponsors-container">
+                        <div className="sponsors-logos">
+                            <a href="https://www.optiver.com/">
+                                <img
+                                    src={optiver_logo}
+                                    className="optiver-logo"
+                                />
+                            </a>
+                        </div>
+                        
+                        <div className="sponsors-logos">
+                            <a href="https://www.canva.com/">
+                                <img
+                                    src={canva_logo}
+                                    className="puresecurity-logo"
+                                />
+                            </a>
+                        </div>
+
+                        <div className="sponsors-logos">
+                            <a href="https://sekuro.io/">
+                                <img
+                                    src={sekuro_logo}
+                                    className="puresecurity-logo"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <h1 className="silver-heading">Silver Sponsors</h1>
+                <div className="sponsors-container">
+                    <div className="sponsors-logos">
+                        <a href="https://www.cybersecpeople.com/">
+                            <img
+                                src={cybersecpeople_logo}
+                                className="cybersecpeople-logo"
+                            />
+                        </a>
+                    </div>
+                    <div className="sponsors-logos">
+                        <a href="https://promithic.io/">
+                            <img
+                                src={promithicio_logo}
+                                className="cybersecpeople-logo"
+                            />
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className="sponsors-logos">
-              <a href="https://www.cybersecpeople.com/">
-                <img
-                  src={cybersecpeople_logo}
-                  className="cybersecpeople-logo"
-                />
-              </a>
-            </div>
-            <div className="sponsors-logos">
-              <a href="https://dronesec.com/">
-                <img src={dronesec_logo} className="dronesec-logo" />
-              </a>
-            </div>
-            <div className="sponsors-logos">
-              <a href="https://www.kaspersky.com.au/">
-                <img src={kaspersky_logo} className="kaspersky-logo" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="sponsors-container">
-            <div className="sponsors-logos">
-              <a href="https://pentesterlab.com/">
-                <img src={pentesterlab_logo} className="pentesterlab-logo" />
-              </a>
-            </div>
-            <div className="sponsors-logos">
-              <a href="https://www.commbank.com.au/">
-                <img src={commbank_logo} className="commbank-logo" />
-              </a>
-            </div>
-            <div className="sponsors-logos">
-              <a href="https://pure.security/">
-                <img src={puresecurity_logo} className="puresecurity-logo" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <h1 className="silver-heading">Silver Sponsors</h1>
-        <div className="sponsors-container">
-          <div className="sponsors-logos">
-            <a href="https://www.optiver.com/">
-              <img src={optiver_logo} className="optiver-logo" />
-            </a>
-          </div>
-          <div className="sponsors-logos">
-            <a href="https://www.accenture.com/au-en">
-              <img src={accenture_logo} className="accenture-logo" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
-};
-export default SponsersPage;
+        </Layout>
+    )
+}
+export default SponsersPage
--- src/utils/sponsersInfo.js
+++ src/utils/sponsersInfo.js
@@ -1,57 +1,95 @@
-import atlassian_logo from "../images/Atlassian Logo solid.png";
-import cybersecpeople_logo from "../images/cybersec_people-logo_on_black.png";
-import dronesec_logo from "../images/dronesec-logo.jpg";
-import kaspersky_logo from "../images/kaspersky-logo.png";
-import pentesterlab_logo from "../images/pentesterlab-logo.jpg";
-import commbank_logo from "../images/comm-bank Logo solid.jpg";
-import optiver_logo from "../images/Optiver Logo solid.jpg";
-import accenture_logo from "../images/accenture-logo.png";
-import puresecurity_logo from "../images/Pure Security - Black.jpg";
+/* eslint-disable linebreak-style */
+import atlassian_logo from '../images/Atlassian Logo solid.png'
+import cybersecpeople_logo from '../images/cybersec_people-logo_on_black.png'
+import dronesec_logo from '../images/dronesec-logo.jpg'
+import kaspersky_logo from '../images/kaspersky-logo.png'
+import pentesterlab_logo from '../images/pentesterlab-logo.jpg'
+import commbank_logo from '../images/comm-bank Logo solid.jpg'
+import optiver_logo from '../images/optiver-logo.jpg'
+import accenture_logo from '../images/accenture-logo.png'
+import puresecurity_logo from '../images/Pure Security - Black.jpg'
+
+import afterpay_logo from '../images/afterpay-logo.png'
+import tesserent_logo from '../images/Tesserent-logo.png'
+import fortian_logo from '../images/fortian-logo.png'
+import promithicio_logo from '../images/promithicio-logo.png'
+import canva_logo from '../images/canva-logo.png'
+import sekuro_logo from '../images/sekuro-logo.jpg'
 
 export const sponsersInfo = [
-  {
-    name: "Atlassian Logo",
-    image: atlassian_logo,
-    href: "https://www.atlassian.com/",
-  },
-  {
-    name: "CyberSecPeople Logo",
-    image: cybersecpeople_logo,
-    href: "https://www.cybersecpeople.com/",
-  },
-  {
-    name: "Dronsec Logo",
-    image: dronesec_logo,
-    href: "https://dronesec.com/",
-  },
-  {
-    name: "Kaspersky Logo",
-    image: kaspersky_logo,
-    href: "https://www.kaspersky.com.au/",
-  },
-  {
-    name: "PentesterLab Logo",
-    image: pentesterlab_logo,
-    href: "https://pentesterlab.com/",
-  },
-  {
-    name: "CommBank Logo",
-    image: commbank_logo,
-    href: "https://www.commbank.com.au/",
-  },
-  {
-    name: "Optiver Logo",
-    image: optiver_logo,
-    href: "https://www.optiver.com/",
-  },
-  {
-    name: "Accenture Logo",
-    image: accenture_logo,
-    href: "https://www.accenture.com/au-en",
-  },
-  {
-    name: "Pure Security Logo",
-    image: puresecurity_logo,
-    href: "https://pure.security/",
-  },
-];
+    {
+        name: 'Atlassian Logo',
+        image: atlassian_logo,
+        href: 'https://www.atlassian.com/',
+    },
+    {
+        name: 'Afterpay Logo',
+        image: afterpay_logo,
+        href: 'https://www.afterpay.com/',
+    },
+    {
+        name: 'CommBank Logo',
+        image: commbank_logo,
+        href: 'https://www.commbank.com.au/',
+    },
+    {
+        name: 'Tesserent Logo',
+        image: tesserent_logo,
+        href: 'https://tesserent.com/',
+    },
+    {
+        name: 'Fortian Logo',
+        image: fortian_logo,
+        href: 'https://www.fortian.com.au',
+    },
+    {
+        name: 'Optiver Logo',
+        image: optiver_logo,
+        href: 'https://www.optiver.com/',
+    },
+    {
+        name: 'Canva Logo',
+        image: canva_logo,
+        href: 'https://www.canva.com/',
+    },
+    {
+        name: 'Sekuro Logo',
+        image: sekuro_logo,
+        href: 'https://sekuro.io/',
+    },
+    {
+        name: 'CyberSecPeople Logo',
+        image: cybersecpeople_logo,
+        href: 'https://www.cybersecpeople.com/',
+    },
+    {
+        name: 'PromithicIo Logo',
+        image: promithicio_logo,
+        href: 'https://promithic.io/',
+    },
+    // {
+    //     name: 'Dronsec Logo',
+    //     image: dronesec_logo,
+    //     href: 'https://dronesec.com/',
+    // },
+    // {
+    //     name: 'Kaspersky Logo',
+    //     image: kaspersky_logo,
+    //     href: 'https://www.kaspersky.com.au/',
+    // },
+    // {
+    //     name: 'PentesterLab Logo',
+    //     image: pentesterlab_logo,
+    //     href: 'https://pentesterlab.com/',
+    // },
+    // {
+    //     name: 'Accenture Logo',
+    //     image: accenture_logo,
+    //     href: 'https://www.accenture.com/au-en',
+    // },
+    // {
+    //     name: 'Pure Security Logo',
+    //     image: puresecurity_logo,
+    //     href: 'https://pure.security/',
+    // },
+]
